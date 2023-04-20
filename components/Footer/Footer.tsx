52d89c7e4e5b636c7cee3e8428897f0315ae9cd8
uellenberg <JonahUellenberg@hotmail.com>
2023-04-20 14:13:44 -0700

    Add more content to homepage

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Flex } from "@chakra-ui/react";
 
 const Footer: React.FC = () => {
-    return <div></div>;
+    return (
+        <Flex flexDir="column">
+            <Box h="100%" textAlign="center">
+                Questions? Email: nsccs@seattlecolleges.edu
+            </Box>
+            
+            <Box flexGrow={1} />
+            
+            <Box h="100%" textAlign="center">
+                Discord
+            </Box>
+        </Flex>
+    );
 };
 
 export default Footer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,53 @@
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Center, Heading, Text } from "@chakra-ui/react";
 
 const Index = () => {
     return (
         <Flex>
             <Header />
 
-            <Box flexGrow={1}>{/* <Body /> */}</Box>
+            <Box flexGrow={1}>
+                <Box w="100%" h={{base: "30vh", md: "50vh", lg: "75vh", xl: "90vh"}}>
+                    {/* Insert background image set to object-fit: cover */}
+                    
+                    <Center mt="50%" w="100%">
+                        <Heading as="h1" pb="50px">Heading Text</Heading>
+                        <Link href="/">
+                            <Text
+                                as="span"
+                                fontSize={{
+                                    base: "1.2em",
+                                    sm: "1.3em",
+                                    md: "1.4em",
+                                    lg: "1.7em",
+                                }}
+                                color="white"
+                                bg="#004da8"
+                                p="10px"
+                                borderRadius="50px"
+                                _hover={{
+                                    bg: "black",
+                                    color: "#95ca59",
+                                }}
+                            >
+                                Join The Club!
+                            </Text>
+                        </Link>
+                    </Center>
+                </Box>
+                
+                <Box py="20px" px="30px" bg="white">
+                    <Heading as="h2" color="#698a2c" pb="30px" textAlign="center">Who We Are</Heading>
+                    <Text pb="30px" textAlign="center">
+                        Example text
+                    </Text>
+                </Box>
+                
+                <Box py="30px" px="20px" bg="#004da8">
+                    <Center w="100%" h="100%">Unfinished - what to put here while we don't have a news system?</Center>
+                </Box>
+            </Box>
 
             <Footer />
         </Flex>
