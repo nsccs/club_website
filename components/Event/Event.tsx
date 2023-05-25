b37444bb6bfd575b148de688c2480c57bf86d0c4
uellenberg <JonahUellenberg@hotmail.com>
2023-05-24 22:06:23 -0700

    Improve database functions

--- components/Event/Event.tsx
+++ components/Event/Event.tsx
@@ -1,18 +1,9 @@
-// Typescript interface defines what the prop passed to Event must look like
-interface EventProp {
-    id: string;
-    title: string;
-    description: string;
-    eventDate: string;
-    createdDate: string;
-}
+import { ClubEvent } from "../../database/events";
 
-const Event = (prop: EventProp) => {
+const Event = (prop: ClubEvent) => {
     // TODO: create this event component!
     // see the wireframe for what this page should look like for inspiration
-    return (
-        <h3>{prop.title}</h3>
-    );
+    return <h3>{prop.title}</h3>;
 };
 
 export default Event;
--- database/events.ts
+++ database/events.ts
@@ -1,56 +1,84 @@
-import db from "./db";
+import { pool } from "./db";
 
-export async function selectAllEvents() {
-    let conn;
-    try {
-        conn = await db.pool.getConnection();
-        const rows = await conn.query(
-            "SELECT id, title, description, event_date, created_date FROM events",
-        );
-        // create the object to return to the view
-        const result = rows.map((row) => ({
-            id: row.id,
-            title: row.title,
-            description: row.description,
-            createdDate: row.created_date.toString(),
-            eventDate: row.event_date.toString(),
-        }));
-        return result;
-    } catch (err) {
-        console.log(err);
-    } finally {
-        if (conn) await conn.release();
-    }
+/**
+ * An event that the club is hosting (or otherwise displaying on the website).
+ */
+export interface ClubEvent {
+    /**
+     * A numerical, unique, immutable ID for this event.
+     */
+    id: number;
+
+    /**
+     * The unique URL slug for the event.
+     * This is how the event is identified by any site visitors.
+     */
+    url: string;
+
+    /**
+     * The event's title / headline.
+     */
+    title: string;
+
+    /**
+     * A description of the event.
+     */
+    description: string;
+
+    /**
+     * The datetime when the event will begin.
+     */
+    eventDate: string;
+
+    /**
+     * The datetime when the event was created (in the database).
+     */
+    createdDate: string;
 }
 
-export async function insertEvent(eventId: string, title: string, description: string, eventDate: string) {
-    let conn;
-    try {
-        conn = await db.pool.getConnection();
-        // Insert query with parameter placeholders
-        const insertQuery =
-            "INSERT INTO events (id, title, description, eventDate) VALUES (?, ?, ?, ?)";
-        // Insert new customer record using pool.query function
-        const result = await conn.query(insertQuery, [
-            eventId,
-            title,
-            description,
-            eventDate,
-        ]);
-        // TODO: result here? what is it?
-        return result;
-    } catch (err) {
-        console.log(err);
-    } finally {
-        if (conn) await conn.release();
-    }
+// TODO: Realistically shouldn't be used as events might become very large; instead, opt ot sort on the database (such as sorting by event_date in order to facilitate the "up-coming events" page).
+/**
+ * Looks up and retrieves every event.
+ */
+export async function selectAllEvents(): Promise<ClubEvent[]> {
+    const rows = await pool.query(
+        "SELECT id, url, title, description, event_date, created_date FROM events",
+    );
+    // create the object to return to the view
+    return rows.map((row) => ({
+        id: row.id,
+        url: row.url,
+        title: row.title,
+        description: row.description,
+        createdDate: row.created_date.toString(),
+        eventDate: row.event_date.toString(),
+    }));
+}
+
+/**
+ * Creates an event, returning back the event object with its ID.
+ * @param event - is the data for the new event.
+ */
+export async function insertEvent(
+    event: Omit<ClubEvent, "id">,
+): Promise<ClubEvent> {
+    const result = await pool.query(
+        "INSERT INTO events (url, title, description, event_date) VALUES (?, ?, ?, ?)",
+        [event.url, event.title, event.description, event.eventDate],
+    );
+
+    // Return back the full event object, plus the ID.
+    return {
+        ...event,
+        id: Number(result.insertId),
+    };
 }
 
 // TODO: THIS IS UNTESTED
 export async function batchInsertEvents(values: string[][]) {
     let conn;
     try {
-        conn = await db.pool.getConnection();
+        conn = await pool.getConnection();
         // Insert query with parameter placeholders
         const insertQuery =
             "INSERT INTO events (first_name, last_name, email) VALUES ?";
@@ -64,39 +92,21 @@ export async function batchInsertEvents(values: string[][]) {
     }
 }
 
-export async function deleteEvent(eventId: string) {
-    let conn;
-    try {
-        conn = await db.pool.getConnection();
-        // Update contact data
-        const result = await db.pool.query("DELETE FROM events WHERE id = ?", [eventId]);
-        return result;
-    } catch (err) {
-        console.log(err);
-    } finally {
-        if (conn) await conn.release();
-    }
+/**
+ * Deletes an event by its ID.
+ * @param id - is the ID of the event to delete.
+ */
+export async function deleteEvent(id: string): Promise<void> {
+    await pool.query("DELETE FROM events WHERE id = ?", [id]);
 }
 
-async function updateEvent(
-    eventId: string,
-    title: string,
-    description: string,
-    eventDate: string,
-) {
-    let conn;
-    try {
-        // Acquire a connection from the connection pool
-        conn = await db.pool.getConnection();
-        // Update event data with passed parameters
-        const result = await conn.query(
-            "UPDATE events SET title = ? description = ? event-date WHERE id = ?",
-            [title, description, eventDate, eventId],
-        );
-        console.log(result);
-    } catch (err) {
-        console.log(err);
-    } finally {
-        if (conn) await conn.release();
-    }
+/**
+ * Updates an event's data.
+ * @param event - is the new data of the event, as well as the ID of the event to update.
+ */
+async function updateEvent(event: ClubEvent): Promise<void> {
+    await pool.query(
+        "UPDATE events SET url=?, title=?, description=?, event_date=? WHERE id = ?",
+        [event.url, event.title, event.description, event.eventDate],
+    );
 }
