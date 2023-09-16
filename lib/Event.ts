5ee8fdd818a89d5686925f1060f8e2f077d76387
uellenberg <JonahUellenberg@hotmail.com>
2023-09-15 22:20:42 -0700

    Partially implement event API

--- lib/Event.ts
+++ lib/Event.ts
@@ -1,12 +1,13 @@
 /** The data needed for an event card. */
 export interface EventCard {
-    /** The event item's ID/URL slug (in Bevy). */
-    id: string;
+    /** The event item's ID/URL slug (in Bevy) for the short URL. */
+    slugID: string;
 
+    // TODO: Implement images.
     /** The URL of this event's image. */
-    image: string | null;
+    // image: string | null;
 
-    /** The date when the event item was posted as a unix timestamp. */
+    /** The date when the event item starts as a unix timestamp. */
     date: number;
 
     /** The event item's title/headline. */
@@ -14,6 +15,117 @@ export interface EventCard {
 
     /** The event item's description. */
     description: string;
+}
+
+/** Event data that needs to be stored internally but doesn't need to be given to the frontend. */
+export interface EventData extends EventCard {
+    /** The event item's numeric ID (for the Bevy API). */
+    id: number;
+}
+
+/** The URL to request a list of published events from. */
+const REQUEST_EVENTS_URL =
+    "https://gdsc.community.dev/api/event/?chapter=1386&status=Published&order_by=-start_date&include_cohosted_events=true";
+
+/** The data returned from {@link REQUEST_EVENTS_URL}. */
+interface RequestEventsData {
+    pagination: {
+        // I think, at the moment it's null.
+        next_page: string | null;
+    };
+    results: {
+        id: number;
+        title: string;
+        /** ISO */
+        start_data: string;
+        /** ISO */
+        end_data: string;
+        url: string;
+    }[];
+}
+
+/**
+ * Gets a list of "partial" events.
+ * These events lack a description, image, and slugID, which must be filled in by requesting
+ * data for each event manually.
+ *
+ * If an error occurs, null is returned.
+ */
+async function getPartialEvents(): Promise<
+    Omit<EventData, "description" | "image" | "slugID">[] | null
+> {
+    try {
+        const data = (await fetch(REQUEST_EVENTS_URL).then((res) =>
+            res.json(),
+        )) as RequestEventsData;
+
+        return data.results.map((result) => ({
+            id: result.id,
+            title: result.title,
+            // Date.parse returns a timestamp with milliseconds.
+            date: Math.floor(Date.parse(result.start_data) / 1000),
+        }));
+    } catch (e) {
+        console.error(e);
+        return null;
+    }
+}
+
+/** The URL to prepend to an event's ID to get its data. */
+const GET_EVENT_URL_PRE = "https://gdsc.community.dev/api/event/";
+
+/** The data returned from {@link GET_EVENT_URL_PRE}. */
+interface GetEventData {
+    description_short: string;
+    short_id: string;
+    event_type_logo: {
+        url: string;
+    };
+}
+
+/**
+ * "Fixes" partial events so they have all the required data.
+ *
+ * If an error occurs, null is returned.
+ * @param partialEvents - is the list of partial events to "fix".
+ */
+async function fixPartialEvents(
+    partialEvents: Omit<EventData, "description" | "image" | "slugID">[],
+): Promise<EventData[] | null> {
+    try {
+        const events: EventData[] = [];
+
+        // This is cached, so we aren't spamming the API.
+        for (const partialEvent of partialEvents) {
+            const data = (await fetch(GET_EVENT_URL_PRE + partialEvent.id).then(
+                (res) => res.json(),
+            )) as GetEventData;
+
+            events.push({
+                ...partialEvent,
+                description: data.description_short,
+                slugID: data.short_id,
+            });
+        }
+
+        return events;
+    } catch (e) {
+        console.error(e);
+        return null;
+    }
+}
+
+/**
+ * Converts an {@link EventData} to an {@link EventCard}.
+ * @param data - is the data to convert.
+ */
+function dataToCard(data: EventData): EventCard {
+    return {
+        slugID: data.slugID,
+        date: data.date,
+        title: data.title,
+        description: data.description,
+    };
 }
 
 /**
