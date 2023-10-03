b44158c77b7e05b389891a31b1e113642b44a7c4
uellenberg <JonahUellenberg@hotmail.com>
2023-10-03 16:34:41 -0700

    Add mock redis

---
+++ lib/MockRedis.ts
@@ -0,0 +1,16 @@
+/**
+ * Creates a simple mock data store for development purposes.
+ * This is API compatible with redisClient for only things actually used
+ * by the website.
+ */
+export class MockRedis {
+    store: Record<string, string> = {};
+
+    public get(key: string): string {
+        return this.store[key] ?? null;
+    }
+
+    public setex(key: string, _: never, value: string) {
+        this.store[key] = value;
+    }
+}
--- lib/redis.ts
+++ lib/redis.ts
@@ -1,6 +1,10 @@
 import Redis from "ioredis";
+import { MockRedis } from "./MockRedis";
 
-export const redisClient = new Redis();
+export const redisClient =
+    process.env.NODE_ENV === "production"
+        ? new Redis()
+        : (new MockRedis() as unknown as Redis);
 
 export const SECOND = 1;
 export const MINUTE = SECOND * 60;
