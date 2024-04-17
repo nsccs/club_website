6c4fce4ac8dc740f51c75bb680417bd8d65b9b54
TheDarkThief <thedarkthief@mailfence.com>
2024-04-16 18:18:18 -0700

    Added F.A.Q to Impactathon page

--- app/(specialPages)/impactathon/page.tsx
+++ app/(specialPages)/impactathon/page.tsx
@@ -327,6 +327,262 @@ const Impactathon = () => {
                         </span>
                     </Link>
                 </Flex>
+                <div
+                    className={css({
+                        display: "flex",
+                        pt: "20px",
+                        pb: "75px",
+                        justifyContent: "center",
+                        flexDir: "column",
+                        marginX: "15%",
+                        "& a": {
+                            color: "cyan.500",
+                        },
+                        "& a:hover": {
+                            textDecoration: "underline",
+                        },
+                        "& h3": { paddingTop: "15px" },
+                    })}
+                >
+                    <h2
+                        className={css({
+                            fontSize: "3xl",
+                        })}
+                    >
+                        F.A.Q:
+                    </h2>
+                    <hr />
+                    <ul className={css({ fontSize: "xl" })}>
+                        <li>
+                            <h3>What is a Hackathon?</h3>
+                            <p>
+                                A hackathon is an event where individuals come
+                                together to collaborate on projects, typically
+                                related to software development or
+                                problem-solving, over a short period of time,
+                                usually lasting a day or a weekend.
+                            </p>
+                        </li>
+                        <li>
+                            <h3>What is the Impactathon event?</h3>
+                            <p>
+                                The Impactathon is a two-day hackathon event
+                                hosted by the North Seattle College Impactful
+                                Investing and Computer Science Clubs that brings
+                                together students, professionals, and activists
+                                to address pressing social justice issues using
+                                data and technology to create solutions.
+                            </p>
+                        </li>
+                        <li>
+                            <h3>Who can participate in the Impactathon?</h3>
+                            <p>
+                                The Impactathon is open to anyone passionate
+                                about social justice and technology-driven
+                                change. Whether you{"'"}re a student,
+                                professional, activist, or community member, you
+                                {"'"}re welcome to join us!
+                            </p>
+                        </li>
+                        <li>
+                            <h3>Do I need technical skills to participate?</h3>
+                            <p>
+                                No technical skills are required to participate
+                                in the Impactathon. We welcome participants with
+                                diverse backgrounds and interests, including
+                                coding, design, communication, and activism.
+                            </p>
+                        </li>
+                        <li>
+                            <h3>How do I register for the Impactathon?</h3>
+                            <p>
+                                To register for the Impactathon, simply fill out
+                                the
+                                <Link href="https://forms.gle/e2HqxdvS8zNVi4aGA">
+                                    {" "}
+                                    registration form.
+                                </Link>
+                                Registration is free and open to all interested
+                                individuals.
+                            </p>
+                        </li>
+                        <li>
+                            <h3>What happens after I register?</h3>
+                            <p>
+                                After registering for the Impactathon, you{"'"}
+                                ll receive a confirmation email with further
+                                instructions and details about the event. You
+                                {"'"}ll also be added to our mailing list to
+                                receive updates and announcements.
+                            </p>
+                        </li>
+                        <li>
+                            <h3>Can I participate as part of a team?</h3>
+                            <p>
+                                Yes, participants are encouraged to form teams
+                                of 2-4 individuals. You can either come with a
+                                pre-formed team or join a team at the event.
+                            </p>
+                        </li>
+                        <li>
+                            <h3>What will I be doing at the Impactathon?</h3>
+                            <p>
+                                During the Impactathon, participants will
+                                collaborate on projects aimed at addressing
+                                social justice issues using data and technology.
+                                You{"'"}ll have the opportunity to attend
+                                workshops, network with industry professionals,
+                                and work on hands-on projects.
+                            </p>
+                        </li>
+                        <li>
+                            <h3>
+                                Will participants be expected to prepare a
+                                presentation or demo for their project?{" "}
+                            </h3>
+                            <p>
+                                Yes, participants are expected to prepare either
+                                a PowerPoint presentation or a live demo to
+                                showcase their project to the judges and other
+                                attendees. Presentations should effectively
+                                communicate the problem being addressed, the
+                                solution developed, and any key features or
+                                functionalities of the project. Demos should
+                                demonstrate the project{"'"}s functionality and
+                                highlight its innovative aspects. Teams will
+                                have an opportunity to present their projects
+                                during the designated presentation/demo sessions
+                                at the hackathon
+                            </p>
+                        </li>
+                        <li>
+                            <h3>
+                                Is there a fee to participate in the
+                                Impactathon?
+                            </h3>
+                            <p>
+                                No, participation in the Impactathon is free of
+                                charge and meals will be provided. However, we
+                                may offer optional add-ons or merchandise for
+                                purchase.
+                            </p>
+                        </li>
+                        <li>
+                            <h3>Will there be prizes for winning projects?</h3>
+                            <p>
+                                Yes, there will be prizes awarded to winning
+                                projects at the Impactathon. Prizes may include
+                                cash awards and other exciting rewards.
+                            </p>
+                        </li>
+                        <li>
+                            <h3>
+                                How can I get involved as a mentor or sponsor?
+                            </h3>
+                            <p>
+                                If you{"'"}re interested in volunteering as a
+                                mentor or sponsoring the Impactathon, please
+                                fill out the mentor{" "}
+                                <Link
+                                    href={"https://forms.gle/RtPikWQS2dADvzpS6"}
+                                >
+                                    registration form
+                                </Link>
+                                . Sponsors please email
+                                <Link href="mailto:impactfulinvesting@seattlecollges.edu">
+                                    {" "}
+                                    impactfulinvesting@seattlecollges.edu
+                                </Link>
+                            </p>
+                        </li>
+                        <li>
+                            <h3>
+                                What can I do to increase my chances of success?
+                            </h3>
+                            <ol
+                                className={css({
+                                    listStyle: "upper-roman outside",
+                                    width: "90%",
+                                    marginX: "auto",
+                                    "& li": {
+                                        paddingTop: "10px",
+                                    },
+                                })}
+                            >
+                                <li>
+                                    <p>
+                                        Familiarize yourself with relevant
+                                        technologies and tools: Take some time
+                                        to learn about the programming
+                                        languages, frameworks, or software that
+                                        might be used during the hackathon. This
+                                        will help you contribute effectively to
+                                        your team{"'"}s project.
+                                    </p>
+                                </li>
+                                <li>
+                                    <p>
+                                        Form or join a team: Hackathons often
+                                        involve teamwork, so consider forming or
+                                        joining a team with individuals who have
+                                        complementary skills and interests.
+                                        Collaborating with others can enhance
+                                        your creativity and problem-solving
+                                        abilities.
+                                    </p>
+                                </li>
+                                <li>
+                                    <p>
+                                        Brainstorm ideas: Think about potential
+                                        project ideas or problems you{"'"}d like
+                                        to address during the hackathon. Come
+                                        prepared with a few concepts that you
+                                        can discuss with your team and
+                                        potentially develop into a project.
+                                    </p>
+                                </li>
+                                <li>
+                                    <p>
+                                        Get plenty of rest: Hackathons can be
+                                        intense and require a lot of energy and
+                                        focus. Make sure to get enough rest
+                                        leading up to the event so that you{"'"}
+                                        re well-rested and ready to tackle
+                                        challenges during the hackathon.
+                                    </p>
+                                </li>
+                                <li>
+                                    <p>
+                                        Bring necessary supplies: Pack any
+                                        essential items you{"'"}ll need for the
+                                        hackathon, such as a laptop, chargers,
+                                        notebooks, pens, snacks, and water.
+                                        Having everything you need on hand will
+                                        help you stay productive throughout the
+                                        event.
+                                    </p>
+                                </li>
+                                <li>
+                                    <p>
+                                        Stay open-minded and flexible:
+                                        Hackathons often involve pivoting and
+                                        adapting to new challenges or ideas.
+                                        Stay open-minded and be willing to
+                                        explore different approaches or
+                                        solutions, even if they{"'"}re different
+                                        from your initial plans.
+                                    </p>
+                                </li>
+                            </ol>
+                        </li>
+                    </ul>
+                    <hr className={css({ margin: "20px" })} />
+                    <p>
+                        By following these tips, you{"'"}ll be better prepared
+                        to participate in the hackathon and make the most of the
+                        experience.
+                    </p>
+                </div>
                 <Box flexGrow={1} />
             </Flex>
             <footer>
